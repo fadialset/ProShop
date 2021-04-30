1f3f45fc70ac28a4814e9758a4da9a6e8e176bd0
fadi <fadi.alset.94@gmail.com>
2021-04-30 17:29:55 +0200

    saved token and added user registration and password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/usermodel.js'
 import generateToken from '../utils/generateToken.js'
+
 // @desc    Auth user & get toker
 // @route   POST /api/useres/login
 // @acsess  public
@@ -22,6 +23,39 @@ const authUser = asyncHandler(async(req,res,) => {
     }
 })
 
+// @desc    Register a new user
+// @route   POST /api/useres/
+// @acsess  public
+const registerUser = asyncHandler(async(req,res,) => {
+    const { name, email, password} = req.body
+
+    const userExists = await User.findOne({ email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    }else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
+
 
 // @desc    get user profile
 // @route   GET /api/useres/profile
@@ -41,4 +75,4 @@ const getUserProfile = asyncHandler(async(req,res,) => {
         throw new Error('User not found')
     }
 })
-export { authUser, getUserProfile}
+export { authUser, getUserProfile, registerUser}
