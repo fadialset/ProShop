ea7478932c65a4d186e7728ae55d60a30a694b4b
fadi <fadi.alset.94@gmail.com>
2021-05-01 21:45:51 +0200

    finished order screen and payment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider } from 'react-redux'
-import store from './store'
-import './bootstrap.min.css'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import './bootstrap.min.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
